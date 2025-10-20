fcaff83c618733ab1b967dba58e4c09a080ef158
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-10-20 23:57:46 +0530

    [ADDED]: shubham This code for DataType in c

---
+++ DatatypeDemo.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+	int a =	10;
+	long b = 20;
+	
+	float x = 40.50;
+	double y = 4000.50;
+	
+	char c = 'A';
+	
+	printf("Integer = %d\n",a);
+	
+	printf("Long =%d\n",b);
+	
+	printf("Float =%f\n",x);
+	
+	printf("Double =%f\n",y);
+	
+	printf("Char = %c\n",c);
+	
+	
+	return 0;
+}
