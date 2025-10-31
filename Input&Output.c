b655248bfc1880f03ff06380262a3dd561115c38
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-10-31 17:27:58 +0530

    [ADDED]: shubham This code for Input & Output Function Demo

---
+++ Input&Output.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+	int a;
+	int b;
+	int c;
+	
+	printf("Enter the first No :");
+	scanf("%d",&a);
+	
+	printf("Enter the second No :");
+	scanf("%d",&b);
+	
+	c = a+b;
+	
+	printf("Addition = %d",c);
+	
+	
+	
+	return 0;
+}
